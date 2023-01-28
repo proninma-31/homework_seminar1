8314687c1439e33ce5aa419ac2cdcefbbc6dfe00
cutulhu <pronin199567@gmail.com>
2023-01-28 13:00:03 +0300

    add task 4 homework

---
+++ task7_homework/Program.cs
@@ -0,0 +1,14 @@
+﻿/Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Вводи число смертный");
+string numberA = Console.ReadLine();
+int A = Convert.ToInt32(numberA);
+int B = 2;
+
+if (A%B==0) {
+	Console.Write($"Число {A} четное");
+}
+else
+{
+	Console.Write($"Число {A} нечетное");
+}
