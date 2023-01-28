7a27526bde0ab7fc065a3da9d3680758f161a9ca
cutulhu <pronin199567@gmail.com>
2023-01-28 13:04:09 +0300

    add task 4 homework

---
+++ task8_homework/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+for (int i = 2; i <= number; i++)
+{
+   if (i%2==0){
+      Console.Write($" {i} " );
+   }
+}
