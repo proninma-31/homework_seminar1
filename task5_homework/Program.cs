4287b116ca699e4475dcd8e77ec3d3590b45bf4e
cutulhu <pronin199567@gmail.com>
2023-01-28 12:56:05 +0300

    add task 1 homework

---
+++ task5_homework/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Вводи первое число смертный");
+string numberA = Console.ReadLine();
+Console.WriteLine("Теперь второе число");
+string numberB = Console.ReadLine();
+int NumberA =Convert.ToInt32(numberA);
+int NumberB = Convert.ToInt32(numberB);
+
+if (NumberA > NumberB)
+{
+	Console.Write($"Большее число: {NumberA} ");
+}
+else
+{
+	Console.Write($"Большее число:{NumberB} ");
+}
